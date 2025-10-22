4f358f1e27e62ca8006c22209caa241658d5d174
AsifAlishishir <asifali210084@diit.edu.bd>
2025-10-23 01:13:21 +0600

    navbar hamburger responsive done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,17 +12,60 @@ const NavBar = () => {
   };
   return (
     <div className=" py-5 bg-white border-b border-[#E9E9E9]">
-      <div className=" flex flex-col md:flex md:flex-row justify-between items-center gap-7 md:gap-0 container mx-auto px-4 md:px-6 lg:px-8">
-        <div className="flex items-center">
-          <img className="w-6 md:w-10" src="/assets/logo.png" alt="Logo" />
-          <NavLink
-            to="/"
-            className="text-[20px] md:text-[24px] font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent ml-3"
-          >
-            HERO.IO
-          </NavLink>
+      <div className=" flex justify-between items-center gap-7 md:gap-0 container mx-auto px-4 md:px-6 lg:px-8">
+        <div className="flex items-center gap-1">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn px-1 btn-ghost lg:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 w-40 mt-2 p-2 shadow"
+            >
+              <NavLink to="/" className="pb-1.5">
+                Home
+              </NavLink>
+              <NavLink to="/apps" className="pb-1.5">
+                Apps
+              </NavLink>
+              <NavLink to="/installation" className="pb-1.5">
+                Installation
+              </NavLink>
+            </ul>
+          </div>
+          <div className="flex items-center">
+            <img
+              className="hidden md:block w-6 md:w-10"
+              src="/assets/logo.png"
+              alt="Logo"
+            />
+            <NavLink
+              to="/"
+              className="text-[20px] md:text-[24px] font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent md:ml-3"
+            >
+              HERO.IO
+            </NavLink>
+          </div>
         </div>
-        <div className=" flex items-center gap-x-8 font-medium text-[#000000]">
+        <div className=" hidden lg:flex items-center gap-x-8 font-medium text-[#000000]">
           <NavLink to="/" className={getNavLinkClass}>
             Home
           </NavLink>
